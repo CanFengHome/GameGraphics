1f4cbd0c53e7b77313af8dc10fb9373ea8d4e60f
canfeng.chen@microfun.com <canfeng.chen@microfun.com>
2016-12-06 09:06:37 +0800

    工程初始化

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GameGraphics
+<p>
+    参考《Windows游戏开发大师技巧》和《3D游戏开发大师技巧》学习总结
+</p>
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphics
+//
+//  Created by mfp on 2016/12/6.
+//  Copyright © 2016年 ccf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
